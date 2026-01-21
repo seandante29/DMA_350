e7561d97f67a023ebabcc2c904c111f9d0eef49c
Adithya1404 <158585885+Adithya1404@users.noreply.github.com>
2026-01-21 10:23:19 +0530

    Update mux_logic.v

--- rtl/mux_logic.v
+++ rtl/mux_logic.v
@@ -5,7 +5,7 @@ module mux_logic #(parameter WIDTH = 32)
 	input wire link_en,data_done,//added data done for mux logic
 	input wire [5:0] wptr,//input from cmd fsm
 	input wire [31:0] header_in,
-	input wire reg_wr_en,
+	input wire chn_wr_en,
 	//previous data
 	input  wire [31:0] CH_CTRL,
 	input  wire [31:0] CH_INTREN,
@@ -92,8 +92,8 @@ module mux_logic #(parameter WIDTH = 32)
 	//assign LINKHDERR = !(|(HEADER_CMD));
     //	assign mux_out_reg = ((link_en && data_done) || cmd_done  ) ? {concat_cmd,reg_bank_in[63:32],reg_bank_in[31:0]} : reg_bank_in;
 
-	assign mux_out_reg = (reg_wr_en ) ? reg_bank_in 
-	                      :(( link_en && data_done) || cmd_done  ) ?{concat_cmd,reg_bank_in[63:32],reg_bank_in[31:0]} 
+	assign mux_out_reg = (chn_wr_en ) ? reg_bank_in 
+	                      :(( link_en && data_done) || cmd_done  ) ?{concat_cmd,mux_out_reg[63:32],reg_bank_in[31:0]} 
 	                     : mux_out_reg;
 	
 	endmodule
