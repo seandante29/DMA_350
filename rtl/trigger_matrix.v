e67a2c8c42eefa1d138db1dc22ab5bda01903561
Adithya1404 <158585885+Adithya1404@users.noreply.github.com>
2026-01-21 10:24:15 +0530

    Update trigger_matrix.v

--- rtl/trigger_matrix.v
+++ rtl/trigger_matrix.v
@@ -1,4 +1,6 @@
-module trigger_matrix_ (
+module trigger_matrix (
+    // form internal register  
+    input STAT_ERR,
 
     // External Trigger INPUTS (from peripherals)
     input  wire        trig0_req,
@@ -24,43 +26,43 @@ module trigger_matrix_ (
     input  wire [7:0]  src_trigin_sel,    // 0 = trig0, 1 = trig1  for peripheral 1 and 2 respectively
 
     // From CH_DESTRIGINCFG
-    input  wire        use_dst_trigin,
-    input  wire [1:0]  dst_trigin_type,   // 2'b10 = HW
-    input  wire [7:0]  dst_trigin_sel,    // 0 = trig0, 1 = trig1
+    input  wire        use_des_trigin,
+    input  wire [1:0]  des_trigin_type,   // 2'b10 = HW
+    input  wire [7:0]  des_trigin_sel,    // 0 = trig0, 1 = trig1
 
     // From CH_TRIGOUTCFG
     input  wire        use_trigout,
-	input  wire [1:0]  trigout_type,       // 2'b10 = HW
-    input  wire [7:0]  trigout_sel,        // 0 = trig0, 1 = trig1
+ input  wire [1:0]  trigout_type,       // 2'b10 = HW
+    input  wire [5:0]  trigout_sel,        // 0 = trig0, 1 = trig1
 
     // To DMA Channel (REQ view)
     output reg         src_trig_req,
     output reg  [1:0]  src_trig_req_type,
 
-    output reg         dst_trig_req,
-    output reg  [1:0]  dst_trig_req_type,
+    output reg         des_trig_req,
+    output reg  [1:0]  des_trig_req_type,
 
     // From DMA Channel (ACK decisions)
     input  wire        ch_src_ack,
     input  wire [1:0]  ch_src_ack_type,
 
-    input  wire        ch_dst_ack,
-    input  wire [1:0]  ch_dst_ack_type,
+    input  wire        ch_des_ack,
+    input  wire [1:0]  ch_des_ack_type,
 
     // From DMA Channel (Trigger OUT request)
     input  wire        ch_trigout_req,
 
     // To DMA Channel (Trigger OUT ACK)
     output reg         ch_trigout_ack,
-	
-	output reg SRCTRIGINSELERR, DESTRIGINSELERR, TRIGOUTSELERR
+ 
+ output reg SRCTRIGINSELERR, DESTRIGINSELERR, TRIGOUTSELERR
 );
 
     always @(*) begin
         src_trig_req      = 1'b0;
         src_trig_req_type = 2'b00;
-        dst_trig_req      = 1'b0;
-        dst_trig_req_type = 2'b00;
+        des_trig_req      = 1'b0;
+        des_trig_req_type = 2'b00;
 
         trig0_ack         = 1'b0;
         trig0_ack_type    = 2'b00;
@@ -71,39 +73,41 @@ module trigger_matrix_ (
         trig1_out_req     = 1'b0;
 
         ch_trigout_ack      = 1'b0;
-		SRCTRIGINSELERR = 1'b0;
-		DESTRIGINSELERR = 1'b0;
-		TRIGOUTSELERR   = 1'b0;
+        if(!STAT_ERR) begin
+            SRCTRIGINSELERR = 1'b0;
+            DESTRIGINSELERR = 1'b0;
+            TRIGOUTSELERR   = 1'b0;
+        end
 
-		// Source trigger select error
-		if (use_src_trigin &&
-			src_trigin_type == 2'b10 &&
-			src_trigin_sel > 1)
-			SRCTRIGINSELERR = 1'b1;
+  // Source trigger select error
+  if (use_src_trigin &&
+   src_trigin_type == 2'b10 &&
+   src_trigin_sel > 1)
+   SRCTRIGINSELERR = 1'b1;
 
-		// Destination trigger select error
-		if (use_dst_trigin &&
-			dst_trigin_type == 2'b10 &&
-			dst_trigin_sel > 1)
-			DESTRIGINSELERR = 1'b1;
+  // Destination trigger select error
+  if (use_des_trigin &&
+   des_trigin_type == 2'b10 &&
+   des_trigin_sel > 1)
+   DESTRIGINSELERR = 1'b1;
 
-		// Trigger OUT select error
-		if (use_trigout &&
-			trigout_type == 2'b10 &&
-			trigout_sel > 1)
-			TRIGOUTSELERR = 1'b1;
+  // Trigger OUT select error
+  if (use_trigout &&
+   trigout_type == 2'b10 &&
+   trigout_sel > 1)
+   TRIGOUTSELERR = 1'b1;
 
-		// Optional conflict: same trigger for src & dst
-		if (use_src_trigin && use_dst_trigin &&
-			src_trigin_type == 2'b10 &&
-			dst_trigin_type == 2'b10 &&
-			src_trigin_sel == dst_trigin_sel)
-		begin
-			SRCTRIGINSELERR = 1'b1;
-			DESTRIGINSELERR = 1'b1;
-		end
+  // Optional conflict: same trigger for src & des
+  if (use_src_trigin && use_des_trigin &&
+   src_trigin_type == 2'b10 &&
+   des_trigin_type == 2'b10 &&
+   src_trigin_sel == des_trigin_sel)
+  begin
+   SRCTRIGINSELERR = 1'b1;
+   DESTRIGINSELERR = 1'b1;
+  end
 
-			
+   
         // SOURCE trigger routing
         if (use_src_trigin && src_trigin_type == 2'b10 && !SRCTRIGINSELERR) begin
             if (src_trigin_sel == 'b0) begin
@@ -123,20 +127,20 @@ module trigger_matrix_ (
         end
 
         // DESTINATION trigger routing
-        if (use_dst_trigin && dst_trigin_type == 2'b10 && !DESTRIGINSELERR) begin
-            if (dst_trigin_sel == 'b0) begin
-                dst_trig_req      = trig0_req;
-                dst_trig_req_type = trig0_req_type;
+        if (use_des_trigin && des_trigin_type == 2'b10 && !DESTRIGINSELERR) begin
+            if (des_trigin_sel == 'b0) begin
+                des_trig_req      = trig0_req;
+                des_trig_req_type = trig0_req_type;
 
-                trig0_ack         = ch_dst_ack;
-                trig0_ack_type    = ch_dst_ack_type;
+                trig0_ack         = ch_des_ack;
+                trig0_ack_type    = ch_des_ack_type;
             end
-            else if (dst_trigin_sel == 'b1) begin
-                dst_trig_req      = trig1_req;
-                dst_trig_req_type = trig1_req_type;
+            else if (des_trigin_sel == 'b1) begin
+                des_trig_req      = trig1_req;
+                des_trig_req_type = trig1_req_type;
 
-                trig1_ack         = ch_dst_ack;
-                trig1_ack_type    = ch_dst_ack_type;
+                trig1_ack         = ch_des_ack;
+                trig1_ack_type    = ch_des_ack_type;
             end
         end
         // TRIGGER OUT routing (DMA → Peripheral)
@@ -154,4 +158,4 @@ module trigger_matrix_ (
     
 
 endmodule
-
+ 
