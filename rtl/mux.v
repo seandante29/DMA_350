d304bcab7cd4eb72e97116bd4d5ea34e19de801e
SwedhaPragalya <159158522+SwedhaPragalya@users.noreply.github.com>
2026-02-12 10:25:04 +0530

    Update mux.v

--- rtl/mux.v
+++ rtl/mux.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 08.01.2026 12:29:34
-// Design Name: 
-// Module Name: mux
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module mux (
 //input wire clk, resetn,
