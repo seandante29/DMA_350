14a136b72a04288c65ea5f6ecad1912db031d1a3
Adithya1404 <158585885+Adithya1404@users.noreply.github.com>
2026-05-18 15:18:48 +0530

    Update register_bank.v

--- rtl/register_bank.v
+++ rtl/register_bank.v
@@ -12,7 +12,7 @@ module register_bank #(parameter WIDTH = 32,
 	input  wire [NUM_CH*WIDTH-1:0]      wrkregval_rd,
 
 	output wire [WIDTH-1:0] cfg_data_out,
-	output wire [NUM_CH-1:0]       chn_wr_en,
+	//output wire [NUM_CH-1:0]       chn_wr_en,
 
 	output wire [NUM_CH*WIDTH-1:0] cfg_channel_start,
 	output wire [NUM_CH*WIDTH-1:0] cfg_channel_status,
