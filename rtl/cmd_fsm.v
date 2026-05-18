9be431aee3ff39378e8605fcd21704cb94ab43d6
Adithya1404 <158585885+Adithya1404@users.noreply.github.com>
2026-05-18 15:17:28 +0530

    Update cmd_fsm.v

--- rtl/cmd_fsm.v
+++ rtl/cmd_fsm.v
@@ -5,7 +5,7 @@ module cmd_fsm
  (
     input clk, 
     input resetn,
-    input wire rst_posedge,
+    //input wire rst_posedge,
     input pause_cmd,resume_cmd,
     input wire [3:0] ch_prio,
     input [31:0]next_cmd_addr,// fro intern reg
@@ -50,7 +50,7 @@ module cmd_fsm
     reg [7:0] count;   
     reg [7:0] count1;
     reg [7:0] count2;
-    reg rst_posedge_reg;
+    //reg rst_posedge_reg;
     reg  max_transfer_count;
     reg data_done_reg;
     reg link_enable_reg;
@@ -75,11 +75,11 @@ wire [$clog2(DATA_W/8)-1:0] byte_offset;
             data_done_reg <= 'd0;
             link_enable_reg <= 'd0;
             STAT_ERROR_reg <= 'd0;
-            rst_posedge_reg <= 0;
+           // rst_posedge_reg <= 0;
         end
         else begin
             data_done_reg <= data_done;
-            rst_posedge_reg <= rst_posedge;
+            //rst_posedge_reg <= rst_posedge;
             link_enable_reg <= link_enable;
             STAT_ERROR_reg <= STAT_ERROR_PARTSEL;
         end
@@ -225,9 +225,13 @@ always @(posedge clk or negedge resetn) begin
     end
     else begin
         CMD_DONE <= 1;
-        if(temp_wptr == 0)
+        
+        
+        if(current_state == PAUSE)
+            CMD_DONE <= 0;
+        else if(temp_wptr == 0)
             temp_rptr <= 0;
-        if(current_state == IDLE)
+        else if(current_state == IDLE)
         CMD_DONE <= ( (data_done || count2 > 0) ? 0 : 1) ;
         else if (current_state == AR || current_state == COUNT )
          CMD_DONE <= 0;
@@ -406,7 +410,7 @@ end
                     //CMD_DONE <= 0;               
                 end
                 
-                PAUSE : CMD_DONE <= 0;
+               // PAUSE : CMD_DONE <= 0;
                 
                 default:
                 begin
