fd1eba436859a332540e214c813e30fbed2f7ddb
SwedhaPragalya <159158522+SwedhaPragalya@users.noreply.github.com>
2026-05-19 11:02:42 +0530

    Update internal_reg.v

--- rtl/internal_reg.v
+++ rtl/internal_reg.v
@@ -14,7 +14,7 @@ module internal_reg #(parameter WIDTH = 32,
     
     input wire boot_en,
       input wire [29:0] boot_addr,
-    
+    input is_ch0,
  //error signals from data fsm
  input wire AXIRDRESPERR,
  input wire AXIRDPOISERR,
@@ -154,7 +154,7 @@ always @(posedge clk or negedge resetn) begin
         rst_posedge <= 0;
     end
     else begin
-        resetn_posedge <= resetn & ~resetn_d;
+        resetn_posedge <= (resetn & ~resetn_d) & is_ch0;
         resetn_d       <= resetn;
         rst_posedge <= resetn_posedge;
     end
